a4a17a3d5123fcf896e827bc12a75d2847908af0
Roberto Duessmann <roberto.duessmann@gmail.com>
2017-08-30 00:27:59 -0300

    Create dto to product

--- src/main/java/com/roberto/dto/ProductDTO.java
+++ src/main/java/com/roberto/dto/ProductDTO.java
@@ -1,7 +1,29 @@
 package com.roberto.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.roberto.model.Product;
+
 /**
  * Created by roberto on 29/08/17.
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
+
+	private Long id;
+	private String name;
+	private String description;
+	private Long quantity;
+
+	public Product toEntity() {
+		return new Product( id, name, description, quantity );
+	}
+
+	public static ProductDTO of(Product product) {
+		return new ProductDTO( product.getId(), product.getName(), product.getDescription(), product.getQuantity() );
+	}
 }
