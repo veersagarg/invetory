a7e9b6a05178a13c199792e255523e33d5b5d107
Roberto Duessmann <roberto.duessmann@gmail.com>
2017-08-30 00:31:37 -0300

    Added PUT method and used DTO in requests and responses

--- src/main/java/com/roberto/controller/ProductController.java
+++ src/main/java/com/roberto/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.roberto.dto.ProductDTO;
 import com.roberto.model.Product;
 import com.roberto.service.ProductService;
 
@@ -24,22 +25,28 @@ public class ProductController {
 	private ProductService service;
 
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public void save(@RequestBody Product product) {
-		service.save( product );
+	public void save(@RequestBody ProductDTO productDTO) {
+		service.save( productDTO );
 	}
 
-	@RequestMapping(value =  "{productId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Product get(@PathVariable String productId){
-		return service.findOne(Long.parseLong( productId ));
+	@RequestMapping(value = "{productId}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void update(@PathVariable String productId, @RequestBody ProductDTO productDTO) {
+		service.update( Long.parseLong( productId ), productDTO );
 	}
 
 	@RequestMapping(value = "{productId}", method = RequestMethod.DELETE)
 	public void delete(@PathVariable String productId) {
-		service.delete( Long.parseLong( productId ));
+		service.delete( Long.parseLong( productId ) );
+	}
+
+	@RequestMapping(value = "{productId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ProductDTO get(@PathVariable String productId) {
+		return service.findOne( Long.parseLong( productId ) );
 	}
 
 	@RequestMapping(value = "/find/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Product> findByName(@PathVariable String name){
+	public List<ProductDTO> findByName(@PathVariable String name) {
 		return service.findByName( name );
 	}
+
 }
--- src/main/java/com/roberto/service/ProductService.java
+++ src/main/java/com/roberto/service/ProductService.java
@@ -1,10 +1,12 @@
 package com.roberto.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.roberto.dto.ProductDTO;
 import com.roberto.model.Product;
 import com.roberto.repository.ProductRepository;
 
@@ -17,19 +19,27 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 
-	public void save(Product product){
+	public void save(ProductDTO productDTO) {
+		repository.save( productDTO.toEntity() );
+	}
+
+	public void update(Long productId, ProductDTO productDTO) {
+		Product product = productDTO.toEntity();
+		product.setId( productId );
 		repository.save( product );
 	}
 
-	public Product findOne(Long productId){
-		return repository.findOne( productId );
+	public void delete(Long productId) {
+		repository.delete( productId );
 	}
 
-	public void delete(Long productId){
-		repository.delete( productId );
-}
+	public ProductDTO findOne(Long productId) {
+		return ProductDTO.of( repository.findOne( productId ) );
+	}
 
-	public List<Product> findByName(String name) {
-		return repository.findByName( name);
+	public List<ProductDTO> findByName(String name) {
+		List<Product> products = repository.findByName( name );
+		return products.stream().map( product -> ProductDTO.of( product ) )
+				.collect( Collectors.toList() );
 	}
 }
