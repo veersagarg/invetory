5a69a69f52f9f60647067c58ae1893ad4d6a19dc
Roberto Duessmann <roberto.duessmann@gmail.com>
2018-04-07 15:16:10 -0300

    Improve methods from controller to return response entity

--- src/main/java/com/roberto/controller/ProductController.java
+++ src/main/java/com/roberto/controller/ProductController.java
@@ -1,9 +1,8 @@
 package com.roberto.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.roberto.dto.ProductDTO;
-import com.roberto.model.Product;
 import com.roberto.service.ProductService;
 
 /**
@@ -25,28 +23,29 @@ public class ProductController {
 	private ProductService service;
 
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public void save(@RequestBody ProductDTO productDTO) {
-		service.save( productDTO );
+	public ResponseEntity save(@RequestBody ProductDTO productDTO) {
+		return ResponseEntity.ok( service.save( productDTO ) );
 	}
 
 	@RequestMapping(value = "{productId}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public void update(@PathVariable String productId, @RequestBody ProductDTO productDTO) {
-		service.update( Long.parseLong( productId ), productDTO );
+	public ResponseEntity update(@PathVariable String productId, @RequestBody ProductDTO productDTO) {
+		return ResponseEntity.ok( service.update( Long.parseLong( productId ), productDTO ) );
 	}
 
 	@RequestMapping(value = "{productId}", method = RequestMethod.DELETE)
-	public void delete(@PathVariable String productId) {
+	public ResponseEntity delete(@PathVariable String productId) {
 		service.delete( Long.parseLong( productId ) );
+		return ResponseEntity.noContent().build();
 	}
 
 	@RequestMapping(value = "{productId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ProductDTO get(@PathVariable String productId) {
-		return service.findOne( Long.parseLong( productId ) );
+	public ResponseEntity get(@PathVariable String productId) {
+		return ResponseEntity.ok( service.findOne( Long.parseLong( productId ) ) );
 	}
 
 	@RequestMapping(value = "/find/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<ProductDTO> findByName(@PathVariable String name) {
-		return service.findByName( name );
+	public ResponseEntity findByName(@PathVariable String name) {
+		return ResponseEntity.ok( service.findByName( name ) );
 	}
 
 }
